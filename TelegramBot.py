7ac14a0f72c3fbcc1ea0105219bb04e2063ff30e
chamRun <chrm@aut.ac.ir>
2021-12-15 18:46:27 +0330

    improves in Bot

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,6 +1,44 @@
-def main():
+from telegram import Update
+import configparser
+from telegram.ext import Updater, CommandHandler
+
+import numpy
+import pandas
+
+from collections import Counter
+
+HELLO_MS = 'سلام! کلمه‌ای که می‌خوای رو بنویس، تا توی فایل اکسلی که داده بودین ببنیم چه کسایی و هرکدوم چند بار، ازش استفاده کردن.'
+
+def start_handler(update, context):
     
+
+
+
+def main():
+
+    config = configparser.ConfigParser()
+    config.read('token.ini')
+    token = config['telegram']['MyPytBot']
+
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start_handler))
+
+
+
+    df = pd.read_excel('Requests.xlsx')
+
+
+
+
+
+    target_df = df[df['توضیحات'].str.contains('واتساپ')]
+    target_df = target_df['نام درخواست کننده']
+    target_df = np.asanyarray(target_df)
+    res = Counter(target_df)
+
+
 
 if __name__ == '__main__':
     main()
-    
